ca4603d41bcb38aa497f8bdfcfaffbb9d845792a
1aaron <aaronnek1@gmail.com>
2018-10-26 19:37:17 -0500

    added gradle dependency and service for locatin

--- app/src/main/java/com/example/aaron/locationbackgroundexample/Activities/MainActivity.kt
+++ app/src/main/java/com/example/aaron/locationbackgroundexample/Activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.aaron.locationbackgroundexample
+package com.example.aaron.locationbackgroundexample.Activities
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aaron.locationbackgroundexample.R
 
 class MainActivity : AppCompatActivity() {
 
